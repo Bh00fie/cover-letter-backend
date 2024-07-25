fa3db46f447e714d172bf9082732f34696dd4fa5
Bh00fie <Bh00fie@gmail.com>
2024-07-25 23:15:59 +0100

    learning MongoDB and how to create a back end using chatGPT with the support of other resources such as youtube video, for now, I have created an env file and put MongoDB connection string and JWS key

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
