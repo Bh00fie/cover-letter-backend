dc0b17ad0d9d577843923a22957537206f6cfb0a
Bh00fie <Bh00fie@gmail.com>
2024-07-27 00:01:56 +0100

    connected to google cloud, the backend works, now I need to allow the user to create an account and authenticate

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+// MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log('MongoDB connection error:', err));
 
+// Root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the homepage!');
+});
+
+// Auth routes
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
